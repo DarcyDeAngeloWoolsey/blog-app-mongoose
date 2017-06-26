a10ca3ddc64ddffb77cd1eea21b6113949de2cb7
Darcy DeAngelo-Woolsey <Bucketlist2>
2017-06-26 11:50:39 -0400

    First commit

---
+++ models.js
@@ -0,0 +1,46 @@
+const mongoose = require('mongoose');
+
+// this is our schema to represent a blog
+const bloggerSchema = mongoose.Schema({
+    title: {
+        type: String,
+        required: true
+    },
+    content: {
+        type: String,
+        required: true
+    },
+    author: {
+        firstName: String,
+        lastName: String
+    },
+
+});
+
+//generate a human readable string based on the author object
+// we're storing in Mongo.
+bloggerSchema.virtual('authorString').get(function () {
+    return `${this.author.firstName} ${this.author.lastName}`.trim() //does this really need to be trimmed of white space?
+});
+
+
+// this is an *instance method* which will be available on all instances
+// of the model. This method will be used to return an object that only
+// exposes *some* of the fields we want from the underlying data
+bloggerSchema.methods.apiRepr = function () {
+
+    return {
+        id: this._id,
+        name: this.name,
+        content: this.cuisine,
+        author: this.authorString
+    };
+}
+
+// note that all instance methods and virtual properties on our
+// schema must be defined *before* we make the call to `.model`.
+const Blogger = mongoose.model('Blogger', bloggerSchema);
+
+module.exports = {
+    Blogger
+};
